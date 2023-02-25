5d003952b79b611b7dc367e71bc0dd8f6aec55ef
musiur <musiur.opu@gmail.com>
2023-02-26 00:08:22 +0600

    home page design done without responsivness...todo for tommorrow

--- components/Button.js
+++ components/Button.js
@@ -2,7 +2,7 @@ const Button = ({ children, type, onClick }) => {
   return (
     <button
       className={`px-4 py-2 rounded-md text-[14px] ${
-        type === "white" ? "text-[#0891B2] bg-white" : "bg-[#0891B2] text-white"
+        type === "white" ? "text-[#0891B2] bg-none" : "bg-[#0891B2] hover:bg-[#06B6D4] text-white"
       }`}
       onClick={onClick}
     >
