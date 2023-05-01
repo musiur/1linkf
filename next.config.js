bfad022ee0589061e1debf76c5e4853b4276f09a
musiur <musiur.opu@gmail.com>
2023-05-01 23:54:41 +0600

    nextConfig file modified

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     domains: ['images.unsplash.com', '1link.st', 'www.google.com'],
   },
   env: {
-    API_HOST: "https://dark-ruby-llama-ring.cyclic.app"
+    API_HOST: 'https://dark-ruby-llama-ring.cyclic.app',
     // API_HOST: 'http://localhost:8080',
   },
   compiler: {
