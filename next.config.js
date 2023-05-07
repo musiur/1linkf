ad78c5d3e6eb66c7acc460589a8747cbdf12d4c2
Musiur <musiur.opu@gmail.com>
2023-05-07 10:40:02 +0600

    next.config.js

--- next.config.js
+++ next.config.js
@@ -4,13 +4,8 @@ const nextConfig = {
     domains: ['images.unsplash.com', '1link.st', 'www.google.com'],
   },
   env: {
-<<<<<<< HEAD
-    // API_HOST: "https://dark-ruby-llama-ring.cyclic.app"
-    API_HOST: 'http://localhost:8080',
-=======
     API_HOST: 'https://dark-ruby-llama-ring.cyclic.app',
     // API_HOST: 'http://localhost:8080',
->>>>>>> 158abfa2e4f03e9461dab1b23cc0bfee2d69a356
   },
   compiler: {
     removeConsole: true,
